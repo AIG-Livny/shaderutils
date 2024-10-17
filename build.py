bbb3d747d84deeb150c7d2e23f999340d7438d98
Ivan Agibalov <AIG.Livny@gmail.com>
2024-10-17 07:46:54 +0300

    update footer

--- build.py
+++ build.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-REQUIRED_VERSION = '0.4.3'
+
+import copy
+
+def tool_config() -> "mapyr.ToolConfig":
+    tc = mapyr.ToolConfig()
+    tc.MINIMUM_REQUIRED_VERSION = '0.4.5'
 
 def config() -> list["mapyr.ProjectConfig"]:
 
@@ -20,7 +25,7 @@ def config() -> list["mapyr.ProjectConfig"]:
     result.append(debug)
 
     # Release config
-    release = debug.copy()
+    release = copy.deepcopy(debug)
     release.CFLAGS    = ["-Ofast","-flto"]
     release.LINK_EXE_FLAGS = ["-flto"]
 
@@ -31,13 +36,14 @@ def config() -> list["mapyr.ProjectConfig"]:
 
 #-----------FOOTER-----------
 # https://github.com/AIG-Livny/mapyr.git
+try:
+    import mapyr
+except:
+    import requests, os
+    os.makedirs(f'{os.path.dirname(__file__)}/mapyr',exist_ok=True)
+    with open(f'{os.path.dirname(__file__)}/mapyr/__init__.py','+w') as f:
+        f.write(requests.get('https://raw.githubusercontent.com/AIG-Livny/mapyr/master/__init__.py').text)
+    import mapyr
+
 if __name__ == "__main__":
-    try:
-        import mapyr
-    except:
-        import requests, os
-        os.makedirs(f'{os.path.dirname(__file__)}/mapyr',exist_ok=True)
-        with open(f'{os.path.dirname(__file__)}/mapyr/__init__.py','+w') as f:
-            f.write(requests.get('https://raw.githubusercontent.com/AIG-Livny/mapyr/master/__init__.py').text)
-        import mapyr
-    mapyr.process(config(), REQUIRED_VERSION)
+    mapyr.process(config)
